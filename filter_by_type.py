f05e69ef7552067e4b5ca05e9ce6d7a7ba6a0ced
Chelsea Cabalan <chex239@gmail.com>
2018-02-03 21:28:23 -0600

    Python Assignments

---
+++ filter_by_type.py
@@ -0,0 +1,24 @@
+# Filter by Type Assignment
+
+# Integer
+bI = 455
+if bI > = 100:
+    print ("That's a big number!")
+else:
+    print ("That's a small number!")
+
+# String
+bS = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
+if len(bS) >= 50:
+    print ("Long sentence.")
+else:
+    print ("Short sentence.")
+
+# List
+mL = [3,5,7,34,3,2,113,65,8,89]
+if len(mL) >= 10:
+    print ("Big list!")
+else:
+    print ("Short list.")
+
+# END
