a7fe11a3a70f6a11988346023f479f99c3a85c65
Chelsea Cabalan <chex239@gmail.com>
2018-02-04 15:59:30 -0600

    Python Assignments

---
+++ dict_tuples.py
@@ -0,0 +1,13 @@
+# Dictionary In, Tuples Out Assignment 
+
+my_dict = {
+  "Speros": "(555) 555-5555",
+  "Michael": "(999) 999-9999",
+  "Jay": "(777) 777-7777"
+}
+def tup_list():
+    print (list(my_dict.items()))
+
+tup_list()
+
+# END
