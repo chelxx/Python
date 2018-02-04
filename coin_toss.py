e81ea0da52087420bc6853369c5c44e1c3e6f306
Chelsea Cabalan <chex239@gmail.com>
2018-02-04 13:15:05 -0600

    Python Assignments

---
+++ coin_toss.py
@@ -0,0 +1,17 @@
+# Coin Tosses Assignment
+
+import random
+heads = 0
+tails = 0
+toss = 0
+while toss < 10:
+    cointoss = random.randint(0, 2)
+    if cointoss == 0:
+        tails = tails + 1
+        print ("Throwing a coin and it's... tails! And there's been {} tails so far!").format(tails)
+    else:
+        heads = heads + 1
+        print ("Throwing a coin and it's... heads! And there's been {} heads so far!").format(heads)
+    toss += 1
+
+# END
