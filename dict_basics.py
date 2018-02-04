238845c1389d348f5e6d6da78650dd630eaa74aa
Chelsea Cabalan <chex239@gmail.com>
2018-02-04 13:59:47 -0600

    Python Assignments

---
+++ dict_basics.py
@@ -0,0 +1,12 @@
+# Dictionary Basics
+
+def information ():
+    per_info = {"name" : "Chelsea", "age" : 27, "country of birth" : "The Philippines", "favorite language" : "Python"}
+    print ("My name is {}.").format(per_info["name"])
+    print ("My age is {}.").format(per_info["age"])
+    print ("My country of birth is {}.").format(per_info["country of birth"])
+    print ("My favorite language is {}.").format(per_info["favorite language"])
+
+information()
+
+# END
