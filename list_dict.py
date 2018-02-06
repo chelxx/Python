246063c364982c3ece4cd85cf8b519848c015dc0
Chelsea Cabalan <chex239@gmail.com>
2018-02-06 12:35:05 -0600

    Python Assignments

--- list_dict.py
+++ list_dict.py
@@ -1,6 +1,6 @@
 # MAKING DICTIONARIES
 
-name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+names = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
 favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
 
 def make_dict(list1, list2):
@@ -9,6 +9,19 @@ def make_dict(list1, list2):
         new_dict[list1[i]] = list2[i]
     print new_dict
         
-make_dict(name, favorite_animal)
+make_dict(names, favorite_animal)
+
+# Hacker Challenge
+
+from random import choice
+
+names = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins"]
+
+def hacker(list1, list2):
+    results = {x: choice(list2) for x in list1}
+    print results
+
+hacker(names, favorite_animal)
 
 # END
