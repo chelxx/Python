eab021249c2c0a5cd5d94be73fe2ae59bc428a7a
Chelsea Cabalan <chex239@gmail.com>
2018-02-08 20:21:26 -0600

    Python Assignments

---
+++ FlaskFund/form_test/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users', methods=['POST'])
+def users():
+    print "Got Post Info"
+
+    name = request.form['name']
+    email = request.form['email']
+    
+    request.form['name']
+    request.form['email']
+
+    print request.form
+
+    return redirect('/')
+
+app.run(debug=True)
